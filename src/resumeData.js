a8f7d0cb132f096087d877960ac95058d599e882
hackstreetguy <omuditgoyal@gmail.com>
2019-06-19 13:25:12 +0530

    initial commit

--- src/resumeData.js
+++ src/resumeData.js
@@ -8,29 +8,29 @@ let resumeData = {
   "socialLinks": [
     {
       "name": "linkedin",
-      "url": "https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+      "url": "https://www.linkedin.com/in/goyalmudit2908/",
       "className": "fa fa-linkedin"
     },
     {
       "name": "github",
-      "url": "http://github.com/rbhatia46",
+      "url": "http://github.com/hackstreetguy",
       "className": "fa fa-github"
     },
     {
       "name": "skype",
-      "url": "http://twitter.com/rbhatia46",
+      "url": "https://twitter.com/GoyalMudit2908",
       "className": "fa fa-twitter"
     }
   ],
-  "aboutme": "I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  "aboutme": "I am currently a pre-final year student at Indian Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Front End Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
   "address": "India",
   "website": "https://rbhatia46.github.io",
   "education": [
     {
-      "UniversityName": "The LNM Insitute of Information Technology",
+      "UniversityName": "Indian Insitute of Information Technology",
       "specialization": "Some specialization",
       "MonthOfPassing": "Aug",
-      "YearOfPassing": "2020",
+      "YearOfPassing": "2022",
       "Achievements": "Some Achievements"
     },
     {
